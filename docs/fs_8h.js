c12f4048c36bb8c5408e83b4995b500528fe6f85
Sebos <sebos@nazarik>
2025-05-20 04:04:25 +0200

    Надеюсь последний штрих по документации

--- docs/fs_8h.js
+++
@@ -1,8 +0,0 @@
-var fs_8h =
-[
-    [ "drayLogPath", "fs_8h.html#a4a32ecc47e89ae484b2b1cbec95cfdf0", null ],
-    [ "initDirs", "fs_8h.html#a791c8dff05a93ea82bde76fc7c4a94ca", null ],
-    [ "is_video_downloaded", "fs_8h.html#a65ecb49246f4d74a183116704b7968a0", null ],
-    [ "log_video_id", "fs_8h.html#ac7135dd56474a60b51b7121ced937914", null ],
-    [ "mkdir_p", "fs_8h.html#ad434ffce2667f992ccfb4b3c200ecd0d", null ]
-];
